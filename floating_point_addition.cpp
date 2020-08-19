bcc0a128f5c8ffd3b9553bf3d5a16f8c977f5760
sohamvg <sohamvg11@gmail.com>
2020-08-19 16:42:27 +0530

    Update assign

--- floating_point_addition.cpp
+++ floating_point_addition.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #define FRACTION_BIT 23
 #define GUARD_BIT 1
 #define ROUND_BIT 1
+#define STICKY_BIT 1
 #define BIAS 127
 
 #define DEBUG 0
@@ -92,32 +93,21 @@ int main(int argc, char *argv[]) {
 
         float resf = bin_to_float(num1_bitset.to_ulong()) + bin_to_float(num2_bitset.to_ulong());
         bitset<SIGN_BIT+EXPONENT_BIT+FRACTION_BIT> resb(float_to_bin(resf));
-        cout << bin_to_float(num1_bitset.to_ulong()) << endl;
-        cout << bin_to_float(num2_bitset.to_ulong()) << endl;
         cout << "Result computed by CPP = " << resf << "  " << resb.to_string() << endl;
 
         bool opposite_signs = num1_sign.to_ulong() != num2_sign.to_ulong() ? true : false;
 
         /*
             significand =
-                <1.><fraction bit><guard bit><round bit>
-                 1          23              1           1
+                <1.><fraction bit><guard bit><round bit><sticky bit>
+                 1          23         1           1       1
         */
 
-        bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT> num1_significand;
-        bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT> num2_significand;
-
-
-        #if DEBUG
-            cout << num1_sign << " " << num1_exponent  << " " << num1_fraction << " " << num1_significand << endl;
-            cout << num1_sign.to_ulong() << " " << num1_exponent.to_ulong() << " " << num1_fraction.to_ulong() << endl;
-
-            cout << num2_sign << " " << num2_exponent  << " " << num2_fraction << " " << num2_significand << endl;
-            cout << num2_sign.to_ulong() << " " << num2_exponent.to_ulong() << " " << num2_fraction.to_ulong() << endl;
-        #endif
+        bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT> num1_significand;
+        bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT> num2_significand;
 
         bitset<SIGN_BIT> result_sign;
-        bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT> result_significand; // One exta bit for carry
+        bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT> result_significand; // One exta bit for carry
         long result_exponent;
 
         // number types
@@ -168,27 +158,34 @@ int main(int argc, char *argv[]) {
             cout << "Num 1 & 2 are denormal" << endl;
             num1_exponent = bitset<EXPONENT_BIT>(1); // Actual exponent is -126
             num2_exponent = bitset<EXPONENT_BIT>(1); // Actual exponent is -126
-            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("0" + num1_fraction.to_string() + "00");
-            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("0" + num2_fraction.to_string() + "00");
+            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("0" + num1_fraction.to_string() + "000");
+            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("0" + num2_fraction.to_string() + "000");
         }
         else if (num1_type == Denormal && num2_type == Normal) {
             cout << "Num 1 is denormal" << endl;
             num1_exponent = bitset<EXPONENT_BIT>(1); // Actual exponent is -126
-            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("0" + num1_fraction.to_string() + "00");
-            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("1" + num2_fraction.to_string() + "00");
+            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("0" + num1_fraction.to_string() + "000");
+            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("1" + num2_fraction.to_string() + "000");
         }
         else if (num1_type == Normal && num2_type == Denormal) {
             cout << "Num 2 is denormal" << endl;
             num2_exponent = bitset<EXPONENT_BIT>(1); // Actual exponent is -126
-            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("1" + num1_fraction.to_string() + "00");
-            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("0" + num2_fraction.to_string() + "00");
+            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("1" + num1_fraction.to_string() + "000");
+            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("0" + num2_fraction.to_string() + "000");
         }
         else
         { // Both num types are normal
-            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("1" + num1_fraction.to_string() + "00");
-            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>("1" + num2_fraction.to_string() + "00");
+            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("1" + num1_fraction.to_string() + "000");
+            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>("1" + num2_fraction.to_string() + "000");
         }
         
+        #if DEBUG
+            cout << num1_sign << " " << num1_exponent  << " " << num1_fraction << " " << num1_significand << endl;
+            cout << num1_sign.to_ulong() << " " << num1_exponent.to_ulong() << " " << num1_fraction.to_ulong() << endl;
+
+            cout << num2_sign << " " << num2_exponent  << " " << num2_fraction << " " << num2_significand << endl;
+            cout << num2_sign.to_ulong() << " " << num2_exponent.to_ulong() << " " << num2_fraction.to_ulong() << endl;
+        #endif
 
         // 1. Compare exponents
         int clock_cycles = 1;
@@ -199,10 +196,18 @@ int main(int argc, char *argv[]) {
 
             for (int i = 0; i < diff; i++)
             {
-                sticky = sticky | num2_significand[i];
+                sticky = sticky | num2_significand[i+STICKY_BIT];
             }
 
-            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>(num2_significand.to_ulong() >> diff);
+            if (diff > 1+FRACTION_BIT+GUARD_BIT+ROUND_BIT) {
+                num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(0);
+            }
+            else {
+                num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(num2_significand.to_ulong() >> diff);
+            }
+
+            string stick_str = sticky == 0 ? "0" : "1";
+            num2_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(num2_significand.to_string().erase(FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT) + stick_str);
             result_exponent = num1_exponent.to_ulong();
         }
         else {
@@ -210,14 +215,25 @@ int main(int argc, char *argv[]) {
 
             for (int i = 0; i < diff; i++)
             {
-                sticky = sticky | num2_significand[i];
+                sticky = sticky | num1_significand[i+STICKY_BIT];
             }
 
-            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT>(num1_significand.to_ulong() >> diff);
+            if (diff > 1+FRACTION_BIT+GUARD_BIT+ROUND_BIT) {
+                num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(0);
+            }
+            else {
+                num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(num1_significand.to_ulong() >> diff);
+            }
+
+            string stick_str = sticky == 0 ? "0" : "1";
+            num1_significand = bitset<1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(num1_significand.to_string().erase(FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT) + stick_str);
             result_exponent = num2_exponent.to_ulong();
         }
 
         #if DEBUG
+            cout << "STEP 1" << endl;
+            cout << "diff = " << diff << " , sticky = " << sticky << endl;
+
             cout << num1_sign << " " << num1_exponent  << " " << num1_fraction << " " << num1_significand << endl;
             cout << num1_sign.to_ulong() << " " << num1_exponent.to_ulong() << " " << num1_fraction.to_ulong() << endl;
 
@@ -230,18 +246,23 @@ int main(int argc, char *argv[]) {
         clock_cycles += 1;
         if (!opposite_signs) {
             result_sign = num1_sign;
-            result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT>(num1_significand.to_ulong() + num2_significand.to_ulong());
+            result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(num1_significand.to_ulong() + num2_significand.to_ulong());
         }
         else {
             if (num1_significand.to_ulong() > num2_significand.to_ulong()) {
                 result_sign = num1_sign;
-                result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT>(num1_significand.to_ulong() - num2_significand.to_ulong());
+                result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(num1_significand.to_ulong() - num2_significand.to_ulong());
             }
             else {
                 result_sign = num2_sign;
-                result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT>(num2_significand.to_ulong() - num1_significand.to_ulong());
+                result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(num2_significand.to_ulong() - num1_significand.to_ulong());
             }
         }
+
+        #if DEBUG
+            cout << "STEP 2: Add significands" << endl;
+            cout << result_sign << " " << result_exponent  << " " << result_significand << endl;
+        #endif
 
         bool still_normalized = false;
         bool underflow = false;
@@ -256,16 +277,18 @@ int main(int argc, char *argv[]) {
                 zero = true;
                 break;
             }
-            else if (result_significand[1+FRACTION_BIT+GUARD_BIT+ROUND_BIT] == 1) {
+            else if (result_significand[1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT] == 1) {
                 // right shift
                 #if DEBUG
                     cout << " shift right" << endl;
                 #endif
+                int new_sticky = result_significand[STICKY_BIT-1] | result_significand[STICKY_BIT];
                 result_significand = result_significand >> 1;
+                result_significand[STICKY_BIT-1] = new_sticky;
                 result_exponent += 1;
             }
             else {
-                if (result_significand[FRACTION_BIT+GUARD_BIT+ROUND_BIT] == 1) {
+                if (result_significand[FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT] == 1) {
                     // Already normalized
                     #if DEBUG
                         cout << "already normalized" << endl;
@@ -277,7 +300,7 @@ int main(int argc, char *argv[]) {
                         cout << "shift left" << endl;
                     #endif
                     int left_shifts = 0;
-                    while (result_significand[FRACTION_BIT+GUARD_BIT+ROUND_BIT] == 0)
+                    while (result_significand[FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT] == 0)
                     {
                         result_significand = result_significand << 1;
                         left_shifts++;
@@ -285,6 +308,11 @@ int main(int argc, char *argv[]) {
                     result_exponent -= left_shifts;
                 }
             }
+
+            #if DEBUG
+                cout << "STEP 3: Normalize" << endl;
+                cout << result_sign << " " << result_exponent  << " " << result_significand << endl;
+            #endif
 
             if (result_exponent - BIAS < -126) {
                 std::cout << "underflow" << endl;
@@ -301,21 +329,32 @@ int main(int argc, char *argv[]) {
             // 4. Rounding
             clock_cycles += 1;
             bool round_up = false;
-            int guard_bit_value = result_significand[GUARD_BIT+ROUND_BIT-1];
-            int round_bit_value = result_significand[ROUND_BIT-1];
+            int guard_bit_value = result_significand[GUARD_BIT+ROUND_BIT+STICKY_BIT-1];
+            int round_bit_value = result_significand[ROUND_BIT+STICKY_BIT-1];
+            int sticky_bit_value = result_significand[STICKY_BIT-1];
+
+            #if DEBUG
+                cout << "Rounding" << endl;
+                cout << "GRS = " << guard_bit_value << " " << round_bit_value << " " << sticky_bit_value << endl;
+            #endif
 
             if (guard_bit_value == 0) {
                 // 0xx = nothing
+                round_up = false;
             }
             else {
-                if (round_bit_value == 0 && sticky == 0) {
+                if (round_bit_value == 0 && sticky_bit_value == 0) {
                     // tie
-                    if (result_significand[GUARD_BIT+ROUND_BIT] == 1) {
+                    #if DEBUG
+                        cout << "Tie breaker case" << endl;
+                    #endif
+                    if (result_significand[1+GUARD_BIT+ROUND_BIT+STICKY_BIT-1] == 1) {
                         // round up
                         round_up = true;
                     }
                     else {
                         // nothing
+                        round_up = false;
                     }
                 }
                 else {
@@ -327,21 +366,19 @@ int main(int argc, char *argv[]) {
             if (round_up) {
                 std::cout << "Rounding up..." << endl;
                 sticky = 0;
-                result_significand[ROUND_BIT-1] = 0;
-                result_significand[GUARD_BIT+ROUND_BIT-1] = 0;
-                bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT> one_bit("100");
+                bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT> one_bit("1000");
 
-                result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT>(result_significand.to_ulong() + one_bit.to_ulong());
+                result_significand = bitset<2+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT>(result_significand.to_ulong() + one_bit.to_ulong());
             }
 
             if (result_significand.to_ulong() == 0) {
                 still_normalized = true;
             }
-            else if (result_significand[1+FRACTION_BIT+GUARD_BIT+ROUND_BIT] == 1) {
+            else if (result_significand[1+FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT] == 1) {
                 still_normalized = false;
             }
             else {
-                if (result_significand[FRACTION_BIT+GUARD_BIT+ROUND_BIT] == 1) {
+                if (result_significand[FRACTION_BIT+GUARD_BIT+ROUND_BIT+STICKY_BIT] == 1) {
                     still_normalized = true;
                 }
                 else {
@@ -350,7 +387,7 @@ int main(int argc, char *argv[]) {
             }
 
             #if DEBUG
-                std::cout << "Still normalized = " << still_normalized << endl; 
+                std::cout << "res significand = " << result_significand.to_string() << ", Still normalized = " << still_normalized << endl; 
             #endif
 
         } while (!still_normalized);
@@ -367,11 +404,20 @@ int main(int argc, char *argv[]) {
             std::cout << "Result = " << zero_bits.to_string() << endl;
         }
         else {
-            std::cout << "result " << result_sign << " " << result_significand << " " << result_exponent - BIAS << endl;
-
-            string result_fraction = result_significand.to_string().erase(FRACTION_BIT+GUARD_BIT+ROUND_BIT, GUARD_BIT+ROUND_BIT).erase(0,2);
+            #if DEBUG
+                cout << "Res significand = " << result_significand.to_string() << endl;
+            #endif
+            string result_fraction = result_significand.to_string().erase(2+FRACTION_BIT).erase(0,2);
+            cout << result_fraction << endl;
             string result = result_sign.to_string() + bitset<EXPONENT_BIT>(result_exponent).to_string() + result_fraction;
             std::cout << "Result = " << result << endl;
+
+            if (result != resb.to_string()) {
+                cout << "wrong !!" << endl;
+            }
+            else {
+                cout << "correct !!" << endl;
+            }
         }
         std::cout << "Clock cycles = " << clock_cycles << endl;
     }
--- test_case_generator.cpp
+++ test_case_generator.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #define MAX_FRACTION 8388607
 
 #define TESTS 100
-#define EXPONENT_TESTS 10
+#define EXPONENT_TESTS 20
 
 int main()
 {
@@ -29,7 +29,7 @@ int main()
     }
     
     ofstream inputfile, ansfile;
-    inputfile.open("rand_inp.txt");
+    inputfile.open("inp_large.txt");
     int count = 0;
     
     for (int i = 0; i < TESTS; i++)
@@ -58,6 +58,30 @@ int main()
 
                 count+=2;
             }
+        }
+    }
+
+    for (int exp1 = 0; exp1 < exponents.size(); exp1++)
+    {
+        string fraction1 = "01010101010101010101010";
+        string fraction2 = "01010101010101010101011";
+
+        for (int exp2 = 0; exp2 < exponents.size(); exp2++)
+        {
+            int exponent1 = exponents[exp1];
+            int exponent2 = exponents[exp2];
+
+            bitset<FRACTION_BIT> f1(fraction1, 0, FRACTION_BIT);
+            bitset<FRACTION_BIT> f2(fraction2, 0, FRACTION_BIT);
+            bitset<EXPONENT_BIT> e1(exponent1);
+            bitset<EXPONENT_BIT> e2(exponent2);
+            bitset<SIGN_BIT+EXPONENT_BIT+FRACTION_BIT> n1("0" + e1.to_string() + f1.to_string(), 0, SIGN_BIT+EXPONENT_BIT+FRACTION_BIT);
+            bitset<SIGN_BIT+EXPONENT_BIT+FRACTION_BIT> n2("0" + e2.to_string() + f2.to_string(), 0, SIGN_BIT+EXPONENT_BIT+FRACTION_BIT);
+            bitset<SIGN_BIT+EXPONENT_BIT+FRACTION_BIT> n3("1" + e2.to_string() + f2.to_string(), 0, SIGN_BIT+EXPONENT_BIT+FRACTION_BIT);
+            inputfile << n1.to_string() << " " << n2.to_string() << endl;
+            inputfile << n1.to_string() << " " << n3.to_string() << endl;
+
+            count+=2;
         }
     }
 
